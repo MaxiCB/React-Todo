e9545a1739b912589d2653a1592ca8185801888c
Aaron Beasley <aaroncbrs@gmail.com>
2019-12-25 01:02:37 -0600

    Updates

---
+++ precache-manifest.fab386facd766a95fe85fbe0fb8224b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84b6ead2b725b02994005b7bf07604d8",
+    "url": "/React-Todo/index.html"
+  },
+  {
+    "revision": "527853e7aa0b30db11e8",
+    "url": "/React-Todo/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "527853e7aa0b30db11e8",
+    "url": "/React-Todo/static/js/2.68750d43.chunk.js"
+  },
+  {
+    "revision": "0083cbebcc954d5e5178",
+    "url": "/React-Todo/static/js/main.6c0c7946.chunk.js"
+  },
+  {
+    "revision": "ae423fafcfc2feba9f35",
+    "url": "/React-Todo/static/js/runtime-main.96b98a16.js"
+  }
+]);
